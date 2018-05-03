a5f604efb5a595dba7aa138bd5399767f43c1843
dannyb21892 <dannyb21892@verizon.net>
2018-05-03 17:47:07 -0400

    ready to work on gameplay

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::MatchesController < ApplicationController
   def index
     @matches = Match.all.select{|match| match.players.length < 5} #use all games and add spectate mode to games in progress
-    puts @matches[0].owner_id
     @owners = @matches.map{|match| Player.find(match.owner_id).username} #or use only games that users can join
     @merged = {data: []}
     @matches.each_with_index{|m, i| @merged[:data].push({match: m, owner: @owners[i]})}
