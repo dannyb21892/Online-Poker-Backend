6b0ac3cea02e35d988b66979536ba50b6aa2df56
dannyb21892 <dannyb21892@verizon.net>
2018-05-03 16:25:14 -0400

    matches controller index and create basic functions

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::MatchesController < ApplicationController
+  def index
+    @matches = Match.all
+    render json: @matches
+  end
+
+  def create
+    @owner = Player.find_by(username: params["owner"])
+    @match = Match.create
+    PlayerSlot.create(match_id: @match.id, player_id: @owner.id)
+    render json: {owner: params["owner"], players: @match.players.map{|player| player.username}, openSlots: (5-@match.players.length)}
+  end
+
 end
