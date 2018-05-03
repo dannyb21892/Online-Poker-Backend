6d9320aa53dc8511cef47392e8ca1024dedc3d59
vsparrow <khan@vsparrow.com>
2018-05-03 13:42:37 -0400

    getting card now from external api

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,10 +9,20 @@ class PlayersController < ApplicationController
     # conn = Faraday.new(:url => 'https://deckofcardsapi.com/api/deck')
     # conn = Faraday.new(:url => 'https://deckofcardsapi.com/api/deck')
     # response = conn.get("/new/shuffle/?deck_count=1")
-    conn = Faraday.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    conn = JSON.parse Faraday.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').body
     # response = conn.get("/new/shuffle/?deck_count=1")
-    STDERR.puts conn.body
-    render json:  conn.body
+    sleep 2
+    deck_id = conn["deck_id"] #bad
+
+    # deck_id =
+    # STDERR.puts `********************#{deck_id}`
+    # card = JSON.parse Faraday.get(`https://deckofcardsapi.com/api/deck/#{deck_id}/draw/?count=1`)
+    card = JSON.parse Faraday.get("https://deckofcardsapi.com/api/deck/#{deck_id}/draw/?count=1").body
+    sleep 2
+    STDERR.puts card
+    # render json:  conn.body
+    # render json:  card["cards"][0]["code"]
+    render json:  card
     # render "HELLO"
     # ren
   end
