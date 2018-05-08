b7f132a17caad817c508139ea16967db235e8a8b
vsparrow <khan@vsparrow.com>
2018-05-08 10:37:34 -0400

    on create Player gets money of 00

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -12,7 +12,16 @@ class Api::V1::PlayersController < ApplicationController
 
   def create
     @player = Player.find_or_create_by(username: params["username"])
+    if @player.money.nil?
+      @player.money = 100
+    end
+    @player.save
       render json: {logged_in: !!@player}
+  end
+
+  def show
+    @player = Player.find_by(username: params["username"])
+    render json: {money: @player.money}
   end
 
   # def login
